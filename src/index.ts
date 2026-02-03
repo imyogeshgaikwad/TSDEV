a6da460426c9da8981dfc9efbd9cff9dc105f25f
BEAST <imyogeshgaikwad@gmail.com>
2026-02-03 13:20:51 +0000

    Initial commit - TypeScript project setup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return`Hello ${person}, Welcome to Typescript`
+}
+
+const username:string = "There"
+console.log(greet(username));
---
+++ src/typesInTs.ts
@@ -0,0 +1,6 @@
+let person =  "There"
+
+let num = Math.random() > 0.5 ? 10 : 5
+
+let channetName = "Yogains"
+
